cf191128f0ab332d9276011d5f1678012071cdae
Neeraj Kumar <nijjukr@gmail.com>
2022-05-08 19:52:15 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={process.env.PUBLIC_URL + '/4.png'} alt='Work in progress' />
+        <h2>Site in progress.......</h2>
+        <a href='https://drive.google.com/file/d/1bOZHyg9wx_-4SZbefZke1FBfufLFNn9k/view?usp=sharing'>Check Resume Here</a>
+      </div>
     </div>
   );
 }
